b37c04bd50f2f2da72bbc1ff327ce5797f077967
TaeHwan <wjd30142@naver.com>
2022-04-13 17:04:19 +0900

    Update ~chap7.3.1

--- workspace/PracticeTobi/src/springbook/user/dao/UserDaoJdbc.java
+++ workspace/PracticeTobi/src/springbook/user/dao/UserDaoJdbc.java
@@ -3,6 +3,7 @@ package springbook.user.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -11,13 +12,23 @@ import org.springframework.jdbc.core.RowMapper;
 
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
+import springbook.user.sqlservice.SqlService;
 
 public class UserDaoJdbc implements UserDao {
+    private JdbcTemplate jdbcTemplate;
+    private Map<String, String> sqlMap;
+    private SqlService sqlService;
+    
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
+    public void setSqlMap(Map<String, String> sqlMap) {
+        this.sqlMap = sqlMap;
+    }
+    public void setSqlService(SqlService sqlService) {
+        this.sqlService = sqlService;
+    }
 
-    private JdbcTemplate jdbcTemplate;
 
     private RowMapper<User> userMapper = new RowMapper<User>() {
         public User mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -35,32 +46,31 @@ public class UserDaoJdbc implements UserDao {
 
     public void add(User user) {
         this.jdbcTemplate.update(
-                "insert into users(id, name, password, email, level, login, recommend) " + "values(?,?,?,?,?,?,?)",
+                this.sqlService.getSql("userAdd"),
                 user.getId(), user.getName(), user.getPassword(), user.getEmail(), user.getLevel().intValue(),
                 user.getLogin(), user.getRecommend());
     }
 
     public User get(String id) {
-        return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[] { id },
+        return this.jdbcTemplate.queryForObject(this.sqlService.getSql("userGet"), new Object[] { id },
                 this.userMapper);
     }
 
     public void deleteAll() {
-        this.jdbcTemplate.update("delete from users");
+        this.jdbcTemplate.update(this.sqlService.getSql("userDeleteAll"));
     }
 
     public int getCount() {
-        return this.jdbcTemplate.queryForInt("select count(*) from users");
+        return this.jdbcTemplate.queryForInt(this.sqlService.getSql("userGetCount"));
     }
 
     public List<User> getAll() {
-        return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
+        return this.jdbcTemplate.query(this.sqlService.getSql("userGetAll"), this.userMapper);
     }
 
     public void update(User user) {
         this.jdbcTemplate.update(
-                "update users set name = ?, password = ?, email = ?, level = ?, login = ?, "
-                        + "recommend = ? where id = ? ",
+                this.sqlService.getSql("userUpdate"),
                 user.getName(), user.getPassword(), user.getEmail(), user.getLevel().intValue(), user.getLogin(),
                 user.getRecommend(), user.getId());
 
